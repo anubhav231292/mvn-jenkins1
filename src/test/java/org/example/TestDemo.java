a72f8f96612f8142f8189bd45441e5b19d3703cd
agarg <anubhav.garg231292@gmail.com>
2021-07-12 16:32:33 +0530

    test demo

---
+++ src/test/java/org/example/TestDemo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDemo {
+
+    @Test
+    public void checkEquals() {
+
+        Assert.assertEquals("test it", "test it");
+
+    }
+}
